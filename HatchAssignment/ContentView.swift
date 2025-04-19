98b034064ed209381879f0c717b4244b6b575094
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-04-19 13:44:41 -0700

    Initial Commit

---
+++ HatchAssignment/ContentView.swift
@@ -0,0 +1,20 @@
+// Created for HatchAssignment in 2025
+// Using Swift 6.0
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HatchAssignment/HatchAssignmentApp.swift
@@ -0,0 +1,13 @@
+// Created for HatchAssignment in 2025
+// Using Swift 6.0
+
+import SwiftUI
+
+@main
+struct HatchAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
