abef2a600cb1bba1ed05fa79bf3dc27d5130114b
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-04-24 13:40:06 -0700

    feat: Add Helper UIScreen+SafeAreaInsets

---
+++ HatchAssignment/Helpers/UIScreen+SafeAreaInsets.swift
@@ -0,0 +1,37 @@
+// Created for HatchAssignment in 2025
+// Using Swift 6.0
+
+import SwiftUI
+
+extension UIApplication {
+    var keyWindow: UIWindow? {
+        connectedScenes
+            .compactMap {
+                $0 as? UIWindowScene
+            }
+            .flatMap {
+                $0.windows
+            }
+            .first {
+                $0.isKeyWindow
+            }
+    }
+}
+
+struct SafeAreaInsetsKey: EnvironmentKey {
+    static var defaultValue: EdgeInsets {
+        UIApplication.shared.keyWindow?.safeAreaInsets.swiftUiInsets ?? EdgeInsets()
+    }
+}
+
+extension EnvironmentValues {
+    var safeAreaInsets: EdgeInsets {
+        self[SafeAreaInsetsKey.self]
+    }
+}
+
+extension UIEdgeInsets {
+    var swiftUiInsets: EdgeInsets {
+        EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
+    }
+}
