7a00e26cf4dd751b0b428d230b0900670efe494c
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-04-26 14:27:15 -0700

    feat: Add Helper - RandomColor

---
+++ HatchAssignment/Helpers/RandomColor.swift
@@ -0,0 +1,14 @@
+// Created for HatchAssignment in 2025
+// Using Swift 6.0
+
+import SwiftUI
+
+extension Color {
+    static var random: Color {
+        return Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
+    }
+}
