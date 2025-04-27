05e06ce3f5c065d6e85aacfc37c298de6f6bdb84
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-04-26 19:28:22 -0700

    feat: Add ViewModifier - ObserveKeyboard

---
+++ HatchAssignment/ViewModifiers/ObserveKeyboard.swift
@@ -0,0 +1,38 @@
+// Created for HatchAssignment in 2025
+// Using Swift 6.0
+
+import SwiftUI
+
+struct ObserveKeyboard: ViewModifier {
+    @Binding var isKeyboardShow: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .onReceive(
+                NotificationCenter.Publisher(
+                    center: .default,
+                    name: UIResponder.keyboardWillShowNotification
+                )
+                .receive(on: RunLoop.main),
+                perform: { _ in
+                    isKeyboardShow = true
+                }
+            )
+            .onReceive(
+                NotificationCenter.Publisher(
+                    center: .default,
+                    name: UIResponder.keyboardWillHideNotification
+                )
+                .receive(on: RunLoop.main),
+                perform: { _ in
+                    isKeyboardShow = false
+                }
+            )
+    }
+}
+
+extension View {
+    func observeKeyboard(_ isKeyboardShow: Binding<Bool>) -> some View {
+        modifier(ObserveKeyboard(isKeyboardShow: isKeyboardShow))
+    }
+}
