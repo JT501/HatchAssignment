b66557ffbdd64652966726e331b70a2eb2ac2725
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-04-29 15:35:15 -0700

    feat: Modularize AppConstants

---
+++ AppModules/Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let targets: [Target] = [
+    .target(
+        name: "AppModules",
+        dependencies: [
+            .appConstants,
+        ]
+    ),
+    .target(name: "AppConstants"),
+]
+
+let package = Package(
+    name: "AppModules",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library("AppModules"),
+        .library("AppConstants"),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/siteline/swiftui-introspect", .upToNextMajor(from: "1.3.0")),
+    ],
+    targets: targets
+)
+
+extension Target.Dependency {
+    // MARK: App Modules
+
+    static var appConstants: Self { .target(name: "AppConstants") }
+
+    // MARK: Third Party Dependencies
+
+    static var introspect: Self { .product(name: "SwiftUIIntrospect", package: "swiftui-introspect") }
+}
+
+// MARK: Helper
+
+extension Product {
+    static func library(_ name: String) -> Product {
+        .library(name: name, targets: [name])
+    }
+}
