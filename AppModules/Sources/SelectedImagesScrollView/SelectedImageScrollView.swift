c695f058d1f6d7ca4e443c2f376245a0314c9f4c
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-04-29 22:34:36 -0700

    feat(SelectedImagescrollView): Update
    SelectedImagescrollView:
    - Extract ThumbnailView into seperate file
    - Use contentMargins for content inset
    
    ThunbnailView
    - Use alignment guide line for close button in Thunbnail View

--- AppModules/Sources/SelectedImagesScrollView/SelectedImageScrollView.swift
+++ AppModules/Sources/SelectedImagesScrollView/SelectedImageScrollView.swift
@@ -20,34 +20,7 @@ public struct SelectedImageScrollView: View {
                 }
             }
         }
-    }
-}
-
-struct ThumbnailView: View {
-    var image: Color
-    var onDidTapDelete: (() -> Void)?
-
-    var body: some View {
-        ZStack(alignment: .topTrailing) {
-            image.frame(width: 50, height: 50)
-                .cornerRadius(5)
-                .padding(8)
-
-            Image(systemName: "x.circle.fill")
-                .frame(width: 15, height: 15)
-                .padding(1)
-                .background(
-                    Circle().fill(.white)
-                )
-                .overlay(
-                    Circle().stroke(.white, lineWidth: 2)
-                )
-                .shadow(radius: 1)
-                .offset(y: 2)
-                .onTapGesture {
-                    onDidTapDelete?()
-                }
-        }
+        .contentMargins(.leading, 8, for: .scrollContent)
     }
 }
 
---
+++ AppModules/Sources/SelectedImagesScrollView/ThumbnailView.swift
@@ -0,0 +1,38 @@
+// Created for AppModules in 2025
+// Using Swift 6.0
+
+import SwiftUI
+
+struct ThumbnailView: View {
+    var image: Color
+    var onDidTapDelete: (() -> Void)?
+
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            image.frame(width: 50, height: 50)
+                .cornerRadius(5)
+
+            Image(systemName: "x.circle.fill")
+                .frame(width: 15, height: 15)
+                .padding(1)
+                .background(
+                    Circle().fill(.white)
+                )
+                .overlay(
+                    Circle().stroke(.white, lineWidth: 2)
+                )
+                .shadow(radius: 1)
+                .alignmentGuide(.top, computeValue: { $0.height / 2 })
+                .alignmentGuide(.trailing, computeValue: { $0.width / 2 })
+                .onTapGesture {
+                    onDidTapDelete?()
+                }
+        }
+    }
+}
+
+#Preview {
+    ThumbnailView(
+        image: .yellow
+    )
+}
