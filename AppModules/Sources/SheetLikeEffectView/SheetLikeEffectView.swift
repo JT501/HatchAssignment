b004b0ec56401c29eedb44b78d1aab4bcbf6b128
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-04-30 22:20:08 -0700

    feat(SheetLikeEffectView): Simplify

--- AppModules/Sources/SheetLikeEffectView/SheetLikeEffectView.swift
+++ AppModules/Sources/SheetLikeEffectView/SheetLikeEffectView.swift
@@ -8,40 +8,28 @@ public struct SheetLikeEffectView<Content: View, Overlay: View>: View {
     @Environment(\.safeAreaInsets) private var safeAreaInsets
 
     public var trigger: Bool
-    public var topPadding: CGFloat?
 
     let content: Content
     let overlay: Overlay
 
     public init(
         trigger: Bool,
-        topPadding: CGFloat? = nil,
         @ViewBuilder content: () -> Content,
         @ViewBuilder overlay: () -> Overlay
     ) {
         self.trigger = trigger
-        self.topPadding = topPadding
         self.content = content()
         self.overlay = overlay()
     }
 
     public var body: some View {
-        ZStack {
-            Color.black
-                .ignoresSafeArea(.all)
-
             ZStack(alignment: .bottom) {
                 content
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .sheetBackgroundEffect(trigger: trigger)
 
                 overlay
-                    .padding(
-                        .top,
-                        trigger ? (topPadding ?? safeAreaInsets.top) : 0
-                    )
             }
+            .background(Color.black)
             .ignoresSafeArea(.all)
-        }
     }
 }
