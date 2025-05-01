e96ff34122b8abb0cb65e0e470f8ce397cfebb1f
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-04-30 21:00:50 -0700

    feat(SelectedImageScrollView): Update
    - Add Scroll To Trailing function
    - Scroll to trailing when a new item inserted
    - Add transition for insertion and removal

--- AppModules/Sources/SelectedImagesScrollView/SelectedImageScrollView.swift
+++ AppModules/Sources/SelectedImagesScrollView/SelectedImageScrollView.swift
@@ -4,23 +4,54 @@
 import SwiftUI
 
 public struct SelectedImageScrollView: View {
+    private static let BottomId = "bottom"
+
     @Binding var selectedImage: [Color]
+    @State var shouldScrollToTrail = false
 
     public init(selectedImage: Binding<[Color]>) {
         _selectedImage = selectedImage
     }
 
     public var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 4) {
-                ForEach(selectedImage, id: \.self) { image in
-                    ThumbnailView(image: image) {
-                        selectedImage.removeAll(where: { $0 == image })
+        ScrollViewReader { reader in
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(selectedImage, id: \.self) { image in
+                        ThumbnailView(image: image) {
+                            selectedImage.removeAll(where: { $0 == image })
+                        }
+                        .transition(
+                            .asymmetric(
+                                insertion: .move(edge: .trailing),
+                                removal: .opacity
+                            )
+                        )
                     }
+
+                    // An invisible spacer for scroll to trailing action
+                    Spacer()
+                        .frame(width: 4)
+                        .id(Self.BottomId)
+                        .onChange(of: shouldScrollToTrail) {
+                            withAnimation(.bouncy(extraBounce: 0.2)) {
+                                reader.scrollTo(Self.bottomId, anchor: .trailing)
+                            }
+                        }
+                }
+                .animation(.bouncy, value: selectedImage)
+            }
+            .scrollIndicators(.hidden)
+            .contentMargins(.horizontal, 8, for: .scrollContent)
+        }
+        .onChange(of: selectedImage) { old, new in
+            if new.count > old.count {
+                Task {
+                    try? await Task.sleep(for: .milliseconds(300))
+                    shouldScrollToTrail.toggle()
                 }
             }
         }
-        .contentMargins(.leading, 8, for: .scrollContent)
     }
 }
 
